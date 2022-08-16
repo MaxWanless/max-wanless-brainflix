b5bff43d00b05ddcc303c017aeb1f8274ee28c14
Max Wanless <maxwanless@gmail.com>
2022-08-16 10:55:13 -0400

    Create Video Data and Desctiption sections

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.scss";
 import Header from "./component/Header/Header";
 import Footer from "./component/Footer/Footer";
+import VideoDescription from "./component/VideoDescription/VideoDescription";
 
 function App() {
   return (
     <>
       <Header />
+      <VideoDescription/>
       <Footer/>
     </>
   );
---
+++ src/component/VideoData/VideoData.js
@@ -0,0 +1,35 @@
+import "./VideoData.scss";
+import viewsIcon from "../../assets/Icons/views.svg";
+import likesIcon from "../../assets/Icons/likes.svg";
+
+const VideoData = (props) => {
+  return (
+    <div className="video-data">
+      <h1 className="video-data__title">BMX Rampage: 2021 Highlights</h1>
+      <div className="video-data__container">
+        <div className="video-data-info">
+          <div className="video-data-info__container">
+            <p className="video-data-info__text video-data-info__text--bold">
+              By Red Crow
+            </p>
+          </div>
+          <div className="video-data-info__container">
+            <p className="video-data-info__text">07/11/2021</p>
+          </div>
+        </div>
+        <div className="video-data-info">
+          <div className="video-data-info__container">
+            <img className="" src={viewsIcon} alt="views icon" />
+            <p className="video-data-info__text">1,001,023</p>
+          </div>
+          <div className="video-data-info__container">
+            <img className="" src={likesIcon} alt="likes icon" />
+            <p className="video-data-info__text">110,985</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoData;
