35c1b7924dadaa5d4b34528af9ae7ca810b49fa9
Max Wanless <maxwanless@gmail.com>
2022-08-16 18:30:24 -0400

    Create VideoPlayer element

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "./App.scss";
 import Header from "./component/Header/Header";
 import Footer from "./component/Footer/Footer";
 import VideoDescription from "./component/VideoDescription/VideoDescription";
+import VideoPlayer from "./component/VideoPlayer/VideoPlayer";
 
 function App() {
   return (
     <>
       <Header />
-      <VideoDescription/>
-      <Footer/>
+      <VideoPlayer />
+      <VideoDescription />
+      <Footer />
     </>
   );
 }
--- src/component/VideoPlayer/VideoPlayer.js
+++ src/component/VideoPlayer/VideoPlayer.js
@@ -1,10 +1,15 @@
 import "./VideoPlayer.scss";
 
 const VideoPlayer = () => {
-  return <video>
-    
-  </video>;
+  return (
+    <div className="player__container">
+      <video
+        className="player"
+        controls
+        poster="https://i.imgur.com/l2Xfgpl.jpg"
+      ></video>
+    </div>
+  );
 };
 
 export default VideoPlayer;
-video
